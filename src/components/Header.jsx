ca96700f7dfbeb95a0b19905b90ee9b3643a1b42
Martin Bednařík <bednarik.mart@gmail.com>
2024-10-18 14:56:43 +0200

    Changed: header html, logic and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
 import SearchInput from "./SearchInput.jsx";
@@ -6,15 +7,40 @@ import SvgIcon from "./SvgIcon.jsx";
 import "../styles/components/Header.scss";
 
 export default function Header({ heading }) {
+  const [isMobileMenuOpened, setIsMobileMenuOpened] = useState(false);
   const mediaGenres = ["all", "movie", "tv"];
+
+  function toggleMenu() {
+    setIsMobileMenuOpened((open) => !open);
+  }
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 900 && isMobileMenuOpened) {
+        setIsMobileMenuOpened(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div className="header">
       <div className="header__content">
-        <div className="header__title">{heading}</div>
-        <div className="header__content-container">
-          {/* <SearchInput /> */}
-          <div className="header__nav-links-wrapper">
+        <div className="header__title-container">
+          <span className="header__title">{heading}</span>
+        </div>
+        <div className="header__user-action">
+          <SearchInput />
+          <ul
+            className={`header__menu ${
+              isMobileMenuOpened ? "header__menu--is-opened" : ""
+            }`}
+          >
             {mediaGenres.map((genre) => {
               return (
                 <NavLink
@@ -30,10 +56,11 @@ export default function Header({ heading }) {
                 </NavLink>
               );
             })}
-          </div>
+          </ul>
           <SvgIcon
             className="header__svg-hamburger-menu"
             iconName={"hamburger-menu"}
+            handleIconClick={toggleMenu}
           />
         </div>
       </div>
--- src/layouts/PageLayout.jsx
+++ src/layouts/PageLayout.jsx
@@ -1,5 +1,11 @@
 import "../styles/layouts/PageLayout.scss";
 
 export default function PageLayout({ children }) {
-  return <div className="page-layout">{children}</div>;
+  return (
+    <div className="page-layout">
+      <div className="page-layout__page-container">
+        {children}
+      </div>
+    </div>
+  );
 }
